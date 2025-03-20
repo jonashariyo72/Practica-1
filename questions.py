5ce5a9e8a0a6e3ef25be00b8c749e85693ed01c7
jonashariyo72 <jonashariyo70@gmail.com>
2025-03-20 08:46:35 -0300

    Funcionalidad 2

--- questions.py
+++ questions.py
@@ -23,6 +23,7 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+puntos = 0 #Para sumar los  puntos
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -41,16 +42,24 @@ for _ in range(3):
                 # Se veifica si la respuesta es correcta
                 if (user_answer - 1) == correct_answers_index[question_index]:
                     print("¡Correcto!")
+                    puntos += 1
+                    print ("Tu puntaje es de: ", puntos)
                     break
                 else:
                     # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
                     print("Incorrecto. La respuesta correcta es:")
                     print(answers[question_index] [correct_answers_index[question_index]])
+                    puntos -= 0.5
+                    print ("Tu puntaje es de: ", puntos)
             else:
                 print("Respuesta no válida.")
+                puntos -= 0.5
+                print ("Tu puntaje es de: ", puntos)
                 sys.exit(1)
         else:
             print("Respuesta no válida.")
+            puntos -= 0.5
+            print ("Tu puntaje es de: ", puntos)
             sys.exit(1)
     # Se imprime un blanco al final de la pregunta
     print()
